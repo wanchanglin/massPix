5a25db6d4a4662bafb1e256e414f5dd0f982db99
wanchanglin <wanchang_lin@outlook.com>
2019-01-30 16:39:49 +0000

    test and help

--- test/massPix.bat
+++ test/massPix.bat
@@ -1,9 +1,9 @@
-rem wl-01-11-2017, Wed: Rscript test code for Windows. More test, see
-rem 'massPix.sh'
+rem wl-01-11-2017, Wed: Rscript test code for Windows.
 
-Rscript --vanilla massPix.R ^
-  --imzML_file "./test-data/test_POS.imzML" ^
+Rscript --vanilla ../massPix.R ^
+  --imzML_file "../test-data/test_POS.imzML" ^
   --process TRUE ^
+  --image_file "../test-data/image_norm.tsv" ^
   --rem_outliers TRUE ^
   --summary FALSE ^
   --pca TRUE ^
--- test/massPix.sh
+++ test/massPix.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # wl-01-11-2017, Wed: Rscript test code for Linux
 
-Rscript --vanilla massPix.R \
-  --imzML_file "./test-data/test_POS.imzML" \
-  --process FALSE \
-  --image_file "./test-data/image_norm.tab" \
+Rscript --vanilla ../massPix.R \
+  --imzML_file "../test-data/test_POS.imzML" \
+  --process TRUE \
+  --image_file "../test-data/image_norm.tsv" \
   --rem_outliers TRUE \
   --summary FALSE \
   --pca TRUE \
-  --pca_out "./test-data/pca.pdf" \
   --loading TRUE \
   --slice TRUE \
  
