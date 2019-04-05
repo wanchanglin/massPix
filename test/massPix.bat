b3b7d8dbc4258a027148dd9f16e0da79f74a0763
harryln <harryln66@hotmail.com>
2019-04-05 10:46:04 +0100

    Names of data set must be sensitive

--- test/massPix.bat
+++ test/massPix.bat
@@ -1,11 +1,22 @@
 rem wl-01-11-2017, Wed: Rscript test code for Windows.
+rem wl-25-03-2019, Mon: add output directory
 
 Rscript --vanilla ../massPix.R ^
-  --imzML_file "../test-data/test_POS.imzML" ^
-  --process TRUE ^
+  --imzML_file "../test-data/test_pos.imzML" ^
   --image_file "../test-data/image_norm.tsv" ^
+  --process TRUE ^
   --rem_outliers TRUE ^
-  --summary FALSE ^
+  --summary TRUE ^
+  --rdata TRUE ^
   --pca TRUE ^
+  --loading TRUE ^
   --slice TRUE ^
   --clus TRUE ^
+  --intensity TRUE ^
+  --image_out "../test-data/res/image.tsv" ^
+  --rdata_out "../test-data/res/r_running.rdata" ^
+  --pca_out "../test-data/res/pca.pdf" ^
+  --loading_out "../test-data/res/loading.xlsx" ^
+  --slice_out "../test-data/res/slice.pdf" ^
+  --clus_out "../test-data/res/clus.pdf" ^
+  --intensity_out "../test-data/res/intensity.tsv"
